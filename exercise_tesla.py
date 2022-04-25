a28bea9f76e25d70a1e3d8897ee69bd1b3f14719
Ashima <89007731+ashima-1018@users.noreply.github.com>
2022-04-25 18:19:33 +0530

    Docstrings added to exerice code

--- exercise_tesla.py
+++ exercise_tesla.py
@@ -1,6 +1,10 @@
+# also used docstring in this code. It give information what our function is doing
 age = input("what is your age?: ")
 
 def checkDriverAge(age=0):  #default value set to 0 
+    '''
+    this function helps to find whether a person is eligable for driving or not
+    '''
     if int(age)<18:
         print("Sorry, you are too young to drive this car. Powering off")
     elif int(age)>18:
@@ -10,3 +14,4 @@ def checkDriverAge(age=0):  #default value set to 0
 
 # calling a functtion
 checkDriverAge()
+
